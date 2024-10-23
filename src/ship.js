abdc02b76c6690d6727b854fa3a1c123c7078dbe
bugbat <85291787+bugbat@users.noreply.github.com>
2024-10-22 21:38:16 -0400

    add sunk status

--- src/ship.js
+++ src/ship.js
@@ -1,14 +1,18 @@
-export function Ship(sz) {
-  let size = sz;
+export function Ship(size) {
   let hits = 0;
+  let sunk = false;
 
   function hit() {
     hits++;
+    isSunk();
   }
 
   function isSunk() {
-    if(hits >= size) return true;
-    else return false;
+    if(hits >= size) {
+      sunk = true;
+      return sunk;
+    }
+    return sunk;
   }
   return { hit, isSunk }
 }
